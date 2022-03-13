8214741ecb420c7b0191260f48f7b5df7361cd85
Richard Carvalho <richardcarvalhooo@gmail.com>
2022-03-13 11:49:13 -0300

    fix: minor route fix

--- src/renderer/src/routes.jsx
+++ src/renderer/src/routes.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
-import { BrowserRouter, Route, Routes as Container } from 'react-router-dom';
+import {
+  BrowserRouter, Route, Routes as Container,
+} from 'react-router-dom';
 import Login from './pages/Login';
 import Homepage from './pages/Homepage';
 
@@ -7,7 +9,7 @@ export default function Routes() {
   return (
     <BrowserRouter>
       <Container>
-        <Route path="/" exact element={<Homepage />} />
+        <Route path="/" element={<Homepage />} />
         <Route path="/login" element={<Login />} />
       </Container>
     </BrowserRouter>
