cac570a169cdaacbb1b8c31005dc73860a2e2d5a
Richard Carvalho <richardcarvalhooo@gmail.com>
2022-03-30 16:38:54 -0300

    style: turning app to NTA colors

--- src/renderer/src/styles/colors.style.js
+++ src/renderer/src/styles/colors.style.js
@@ -1,10 +1,10 @@
 export default {
-  primary: '#3498DB',
+  primary: '#33BEC1',
   secondary: '#FFF',
   danger: '#D64242',
-  header: '#2980B9',
+  header: '#00AEB1',
   placeholder: 'rgba(255, 255, 255, 0.75)',
-  placeholderSecondary: 'rgba(41, 128, 185, 0.75)',
+  placeholderSecondary: 'rgba(51, 190, 193, 0.75)',
   transparentBackground: 'rgba(0, 0, 0, 0.5)',
   completeBackground: '#000',
 };
