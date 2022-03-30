0642c9c894f44c1b0dddead72e63bed9503baa3b
Richard Carvalho <richardcarvalhooo@gmail.com>
2022-03-30 17:21:35 -0300

    feat: adding icon to build

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 const path = require('path');
 
 module.exports = {
@@ -18,6 +19,11 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, 'public/index.html'),
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: 'public/favicon.ico', to: 'icon.ico' },
+      ],
     }),
   ],
   module: {
