0c508e72e0d6ce4694977a307ba85b80637a2663
Richard Carvalho <richardcarvalhooo@gmail.com>
2022-03-29 23:34:40 -0300

    style: color improvements

--- src/renderer/src/styles/colors.style.js
+++ src/renderer/src/styles/colors.style.js
@@ -1,7 +1,7 @@
 export default {
   primary: '#2980B9',
   secondary: '#FFF',
-  danger: '#82322F',
+  danger: '#D64242',
   placeholder: 'rgba(255, 255, 255, 0.75)',
   transparentBackground: 'rgba(0, 0, 0, 0.5)',
   completeBackground: '#000',
